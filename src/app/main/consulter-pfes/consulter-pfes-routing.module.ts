8578de743da5911c5b9259b6ec58684dd611dc99
mo5mami <mami.mokhtar123@gmail.com>
2021-01-30 11:16:43 +0100

    consulter vos pfes fixed

--- src/app/main/consulter-pfes/consulter-pfes-routing.module.ts
+++ src/app/main/consulter-pfes/consulter-pfes-routing.module.ts
@@ -2,13 +2,15 @@ import { ConsulterPFEsComponent } from './consulter-pfes.component';
 
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { PfeCardComponent } from './pfe-card/pfe-card.component';
 
 const routes: Routes = [{
   path: '',
+  component: ConsulterPFEsComponent,
   children:[
   {
     path: '',
-    component: ConsulterPFEsComponent,
+    component: PfeCardComponent,
   }
 ]
   
