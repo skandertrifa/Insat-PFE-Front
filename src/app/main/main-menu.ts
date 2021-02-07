34f287d2e713dba192fee1bef0f53f5000a88f55
bochrasaffar <bochrasaffar@gmail.com>
2021-02-07 18:27:35 +0100

    changed details-pfe behavour based on the user role

--- src/app/main/main-menu.ts
+++ src/app/main/main-menu.ts
@@ -1,5 +1,4 @@
 import { NbMenuService, NbMenuItem } from '@nebular/theme';
-import { AuthService } from '../auth/services/auth.service';
 
  export const MENU_ITEMS: NbMenuItem[] = [
   {
@@ -29,35 +28,3 @@ import { AuthService } from '../auth/services/auth.service';
   },
   
 ];
- class STUDENT_MENU_ITEM {
-
-role :string = '';
-
- 
-studentMenuItem: NbMenuItem[] = [
-  
-    {
-        title: 'Deposer votre PFE',
-        icon: 'edit-2-outline',
-        link: 'deposer-pfe',
-        
-      },
-  ]
-
-
-constructor( private nbMenuService: NbMenuService,     private authService : AuthService,
-    ) {
-    this.addStudentMenuItem();
-  }
-
-  addStudentMenuItem(){
-    const token = localStorage.getItem('token')
-    const payload = this.authService.getDecodedAccessToken(token);
-    console.log(payload)
-    this.role = payload.role
-      
-    if (this.role=="user")
-    {
-      this.nbMenuService.addItems(this.studentMenuItem);
-    }
-  }}
