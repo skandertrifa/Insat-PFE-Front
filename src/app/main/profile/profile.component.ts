483030efcd34223ac2fda9145f24a5f401d31632
attiaahmed143 <attiaahmed143@gmail.com>
2021-02-07 02:28:34 +0100

    Basic user Profile

--- src/app/main/profile/profile.component.ts
+++ src/app/main/profile/profile.component.ts
@@ -1,3 +1,6 @@
+import { Enseignant } from './../../models/enseignant';
+import { AuthService } from './../../services/auth.service';
+import { Student } from './../../models/student';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService : AuthService,
+  ) { }
 
+  user = null;
+  role :string = '';
   ngOnInit(): void {
+    const token = localStorage.getItem('token')
+    const payload = this.authService.getDecodedAccessToken(token);
+    console.log(payload)
+    this.role = payload.role
+    this.user = payload
   }
 
 }
