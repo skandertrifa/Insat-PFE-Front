9da6864566e1524f9eea9f83b192c5c89b277b01
mo5mami <mami.mokhtar123@gmail.com>
2021-02-07 15:29:02 +0100

    catalogue without rapport link done

---
+++ src/app/main/catalogue-pfes/models/catalogue.ts
@@ -0,0 +1,12 @@
+export class Catalogue{
+    id : number
+    titre : string
+    name : string
+    sujetTitre : string
+    sujetDescription : string
+    nomEtudiant : string
+    prenomEtudiant : string
+    filiere : string
+    nomEncadrant : string
+    prenomEncadrant : string
+}
