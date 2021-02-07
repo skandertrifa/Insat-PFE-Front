9ba73a2ec65f83dea1227ead30f564659803c291
mo5mami <mami.mokhtar123@gmail.com>
2021-02-08 00:38:02 +0100

    global fixes

--- src/app/main/deposer-pfe/deposer-pfe.component.ts
+++ src/app/main/deposer-pfe/deposer-pfe.component.ts
@@ -1,9 +1,11 @@
+import { AuthService } from './../../services/auth.service';
 import { DeposerPfeService } from './services/deposer-pfe.service';
 import { NgForm } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
+
 import { Enseignant } from '../../models/enseignant';
 import { EnseignantService } from '../../services/enseignant.service';
+import { NbToastrService } from '@nebular/theme';
 @Component({
   selector: 'ngx-deposer-pfe',
   templateUrl: './deposer-pfe.component.html',
@@ -12,32 +14,58 @@ import { EnseignantService } from '../../services/enseignant.service';
 export class DeposerPFEComponent implements OnInit {
 
   private file:File;
-  description : string;
-  titre : string;
-  selectedEnseignant :Enseignant;
-  selectedEnseignantId : string;
+  description : string="";
+  titre : string="";
+  selectedEnseignant :Enseignant=null;
+  selectedEnseignantId : string="1";
   enseignants : Enseignant[] = [];
 
   constructor(
     private sujetService : DeposerPfeService,
-    private toastrService: ToastrService,
+    private toastrService: NbToastrService,
     private enseignantService : EnseignantService,
+    private authService : AuthService
 
   ) {
   }
 
   ngOnInit(): void {
+    const token = localStorage.getItem('token')
+    const payload = this.authService.getDecodedAccessToken(token);
+ 
+    this.loadSujet(payload.studentDetails.id)
     this.loadEnseignants()
+    
   }
 
   onFileChange(fileChangeEvent) {
     this.file = fileChangeEvent.target.files[0];
   }
 
+  loadSujet(id){
+    this.sujetService.getSujet(id).subscribe(
+      (response) => {
+        console.log("sujet hi : ",response)
+        if(typeof response.titre !==null && typeof response.titre !==undefined)
+          this.titre = response.titre;
+        if(typeof response.description !==null && typeof response.description !==undefined)
+          this.description = response.description;
+        if(typeof response.encadrant !==null && typeof response.encadrant !==undefined)
+          this.selectedEnseignant = response.encadrant
+      })
+  }
+
   loadEnseignants(){
     this.enseignantService.getEnseignants().subscribe(
       (response) => {
         this.enseignants = response;
+        if (typeof this.selectedEnseignant!== null && typeof this.selectedEnseignant!== undefined){
+          for (const enseignant of this.enseignants){
+            if (enseignant.id === this.selectedEnseignant.id){
+              this.selectedEnseignant=enseignant
+            }
+          }
+        }
       })
   }
 
@@ -61,9 +89,11 @@ export class DeposerPFEComponent implements OnInit {
       },
       (error)=>{
         console.log('fail')
-        this.toastrService.error("Echec de l'ajout de sujet");
+        this.toastrService.show("Echec de l'ajout de sujet",{status : "danger"});
       }
     )
   }
 
 }
+
+
