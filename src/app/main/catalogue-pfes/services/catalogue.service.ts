bea8d8027631dd0f0f6173de8b1906f6efca6cfc
mo5mami <mami.mokhtar123@gmail.com>
2021-02-07 21:23:03 +0100

    soutenance catalogue

--- src/app/main/catalogue-pfes/services/catalogue.service.ts
+++ src/app/main/catalogue-pfes/services/catalogue.service.ts
@@ -5,6 +5,7 @@ import { Catalogue } from '../models/catalogue';
 
 
 const SOUTENANCE_API = 'http://127.0.0.1:3000/soutenance';
+const SUJET_API  = 'http://127.0.0.1:3000/sujet';
 
 @Injectable({
   providedIn: 'root'
@@ -15,9 +16,12 @@ export class CatalogueService {
   getData() : Observable<any>{
 
     const result=this.http.get(SOUTENANCE_API+'/catalogue');
-    console.log("result catalogue data : ",result)
+    
     return result
     
+  }
+  downloadRapport(id):Observable<any> {
+    return this.http.get<any>(SUJET_API+'/downloadRapport' + `/${id}`);
   }
   
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { AuthService } from './../services/auth.service';
+import { Component, OnInit } from '@angular/core';
 
 import { MENU_ITEMS } from './main-menu';
+import { NbMenuItem } from '@nebular/theme';
 
 @Component({
   selector: 'ngx-main',
@@ -12,7 +14,70 @@ import { MENU_ITEMS } from './main-menu';
     </ngx-one-column-layout>
   `,
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
 
-  menu = MENU_ITEMS;
+  menu : NbMenuItem[] = []
+  role : string ="role"
+  ngOnInit(){
+    const token = localStorage.getItem('token')
+    const payload = this.authService.getDecodedAccessToken(token);
+ 
+    console.log("payload : ",payload)
+    this.role = payload.role
+    if (this.role==="user"){
+      this.menu = [
+        {
+          title: 'Catalogue PFEs',
+          icon: 'grid-outline',
+          link: 'catalogue-pfes',
+          home:true
+          
+        },
+        {
+          title: 'Consulter vos PFEs',
+          icon: 'layout-outline',
+          link: 'consulter-pfes',
+          
+        },
+        {
+          title: 'Deposer votre PFE',
+          icon: 'edit-2-outline',
+          link: 'deposer-pfe',
+          
+        },
+        {
+          title: 'Profile',
+          icon: 'lock-outline',
+          link: 'profile',
+          
+        },
+        
+      ];
+    } 
+    else {
+      this.menu = [
+        {
+          title: 'Catalogue PFEs',
+          icon: 'grid-outline',
+          link: 'catalogue-pfes',
+          home:true
+          
+        },
+        {
+          title: 'Consulter vos PFEs',
+          icon: 'layout-outline',
+          link: 'consulter-pfes',
+          
+        },
+        {
+          title: 'Profile',
+          icon: 'lock-outline',
+          link: 'profile',
+          
+        },
+        
+      ];
+    }
+  }
+  constructor(private authService : AuthService){}
 }
